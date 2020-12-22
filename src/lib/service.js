8b4d750ffe0f333911919d09ad50ff50cdbd8690
ruysapoly <rumeysaturgutt@gmail.com>
2020-12-22 17:27:36 +0300

    return-is-added

--- src/lib/service.js
+++ src/lib/service.js
@@ -5,12 +5,14 @@ const ENDPOINT = "https://jsonplaceholder.typicode.com";
 
 async function getData (userId) {
     try {
-		const { data: user } = await axios.get(`${ENDPOINT}/users/${userId}`);
+		    const { data: user } = await axios.get(`${ENDPOINT}/users/${userId}`);
         const { data: posts } = await axios.get(`${ENDPOINT}/posts?userId=${userId}`);
         user["post"]= posts;
-        console.log(user);
+        return user;
 	} catch (e) {
 		console.log(e);
 	}
 }
+
+export default { getData };
 
