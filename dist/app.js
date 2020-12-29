5aa76fbba64473186cec83c0c084ce53a1bdd05d
ruysapoly <rumeysaturgutt@gmail.com>
2020-12-29 12:04:56 +0300

    modules reinstalled

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,4 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }//const {getData }= require('./lib/service.js');
-var _service = require('./lib/service'); var _service2 = _interopRequireDefault(_service);
-
-/*let data = x.getData(1);
-
-data.then(function(result){
-  console.log(result);
-});*/
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _service = require('./lib/service'); var _service2 = _interopRequireDefault(_service);
 
 let data = _service2.default.call(void 0, 1);
 data.then(function(result){
