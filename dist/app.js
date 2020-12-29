16dde0e2b9ccf4ef4e5013c1d7c65999902f82b3
ruysapoly <rumeysaturgutt@gmail.com>
2020-12-29 12:12:11 +0300

    gitignore added

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _service = require('./lib/service'); var _service2 = _interopRequireDefault(_service);
-
-let data = _service2.default.call(void 0, 1);
-data.then(function(result){
-    console.log(result);
-  });
-  
